6d1a29c6d832d50eb5aaf2bfeca8c626afb04f41
Adibrill1 <opolit@gmail.com>
2026-03-13 05:14:29 +0200

    Default theme: dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
+    <ThemeProvider attribute="class" defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   );
